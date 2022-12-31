0a251c0172b42c65da3600599aa82aaede0d5c13
Adry Mateo Ramon <77759876+Adrydevmateo@users.noreply.github.com>
2022-12-31 11:18:44 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           I'm a Full Stack Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I&apos;m a full-stack developer specializing in building (and
+          I&apos;m a web developer specializing in building (and
           occasionally designing) exceptional digital experience. Currently,
           I&apos;m focused on building responsive full-stack web applications.
         </p>
