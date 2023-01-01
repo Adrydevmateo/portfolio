5ca42d9e8f6ca079fbc29b5df3623e770ebdf3a1
Adry Mateo Ramon <77759876+Adrydevmateo@users.noreply.github.com>
2023-01-01 12:15:34 -0400

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -112,6 +112,38 @@ const Work = () => {
             </section>
           </article>
           
+          {/* Weather Project */}
+          <article
+            style={{
+              backgroundImage: `url(${"https://github.com/Adrydevmateo/weather_app/blob/master/public/preview.png?raw=true"})`,
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-card hover:scale-105 duration-300"
+          >
+            <section className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Password Generator {">"} TS
+              </span>
+              <section className="pt-8 text-center">
+                <a
+                  href={"https://weather-app-drab-seven.vercel.app/"}
+                  target={"_blank"}
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Adrydevmateo/weather_app"
+                  target={"_blank"}
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </section>
+            </section>
+          </article>
+          
           {/* Next Project */}
         </section>
       </div>
